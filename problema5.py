9d5b6004f89a535e60ed084c79a81eee9cfb679c
Oscar Islas <oislas@gmail.com>
2021-11-18 22:48:08 -0600

    update to python 3

--- problema5.py
+++ problema5.py
@@ -1,15 +1,15 @@
 '''
 http://sg.com.mx/revista/47/cinco-problemas-para-menos-1-hora#.VftNmhrR-N1
 Problema 5
-
 La cadena original es 123456789
-
 1 2 3 4 5 6 7 8 9
- x x x x x x x x
-
+ X X X X X X X X
 en cada X va uno de los siguientes operadores: concatena '',suma '+' o resta '-'
+construyes la cadena concatenando y la evaluas para saber si el resultado
+cumple el requisito de que sea igual a 100
 '''
-cont= 1;
+cien = 100
+cont= 1
 L = ['','+','-']
 for a in L:
  for b in L:
@@ -20,6 +20,6 @@ for a in L:
       for g in L:
        for h in L:
          cadena = "1"+a+"2"+b+"3"+c+"4"+d+"5"+e+"6"+f+"7"+g+"8"+h+"9" 
-         if eval(cadena)==100:
-           print "%i) "%cont + cadena + " = " + "%i"%eval(cadena)
+         if eval(cadena)==cien:
+           print (cadena + " =" , cien)
            cont = cont+1      
